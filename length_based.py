8a5f1c38afa166956f1d9054b922cde62f472c3c
anish1A1 <alloozpat87@gmail.com>
2026-02-09 11:31:16 +0545

    CharacterTextSplitter understood

--- length_based.py
+++ length_based.py
@@ -12,11 +12,14 @@ even certain medical imaging techniques trace their roots back
 to innovations driven by space programs.
 """
 
+loader = PyPDFLoader('dl-curriculum.pdf')
+docs = loader.load()
 splitter = CharacterTextSplitter(
-    chunk_size = 100,
-    chunk_overlap=0,
-    separator=''
+    chunk_size = 500,
+    chunk_overlap=10,
+    separator=' '
 )
 
-result = splitter.split_text(text)
+result = splitter.split_documents(docs)
 print(result)
+print('\n\n',result[0].page_content)
